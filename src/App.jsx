164a4ee8dff4618ba6c6541d9fdd237116173288
Tin <jaytintran@gmail.com>
2024-09-30 01:32:33 +0700

    finished

--- src/App.jsx
+++ src/App.jsx
@@ -11,16 +11,33 @@ function Header() {
 }
 
 function Menu() {
+  const pizzas = pizzaData;
+
   return (
     <main className="menu">
       <h2>Our Menu</h2>
-      <div
-        style={{ display: "grid", gridTemplateColumns: "1fr 1fr", gap: "2rem" }}
-      >
-        {pizzaData.map((pizza, id) => {
-          return <PizzaCard className="menu" key={pizza.name} pizza={pizza} />;
-        })}
-      </div>
+      <p>
+        Authentic italian food. 6 different flavors to choose from. All from our
+        stone oven, all organic, all delicious, and healthy.
+      </p>
+
+      {pizzas.length > 0 ? (
+        <div
+          style={{
+            display: "grid",
+            gridTemplateColumns: "1fr 1fr",
+            gap: "2rem",
+          }}
+        >
+          {pizzaData.map((pizza, id) => {
+            return (
+              <PizzaCard className="menu" key={pizza.name} pizza={pizza} />
+            );
+          })}
+        </div>
+      ) : (
+        <p>We're still working on our menu, please come back later.</p>
+      )}
     </main>
   );
 }
@@ -28,8 +45,12 @@ function Menu() {
 function PizzaCard({ pizza }) {
   const { name, price, photoName, ingredients, soldOut } = pizza;
 
+  // if (soldOut) {
+  //   return null;
+  // }
+
   return (
-    <div className="pizza">
+    <div className={soldOut ? "pizza sold-out" : "pizza"}>
       {/* Make the image responsive and all same size */}
       <figure
         style={{
@@ -51,10 +72,26 @@ function PizzaCard({ pizza }) {
 
       <div>
         <h3>{name}</h3>
-        <h3>{ingredients}</h3>
+        <span
+          style={{ display: "block", fontSize: "1.6rem", fontStyle: "italic" }}
+        >
+          {ingredients}
+        </span>
         <span>Price: ${price}</span>
-        <p>Sold Out: {soldOut ? "Yes" : "No"}</p>
+        <p>{soldOut ? "Sold Out" : pizza.price}</p>
       </div>
+    </div>
+  );
+}
+
+function Order({ greeting, closeHours }) {
+  return (
+    <div className="order">
+      <p>
+        {/* It's {new Date().getHours()} o'clock. {greeting} */}
+        {greeting}, we're open until {closeHours} o'clock`
+      </p>
+      <button className="btn">Order Now</button>
     </div>
   );
 }
@@ -74,23 +111,34 @@ function Footer() {
     greeting = "Good Evening";
   }
 
+  // if (!isOpen) {
+  //   return (
+  //     <p>
+  //       {`Sorry, We're closed.<br />We're happy to welcome you between ${openHours} and ${closeHours} o'clock`}
+  //     </p>
+  //   );
+  // }
+
   return (
     <footer className="footer">
-      <p>
-        It's {new Date().getHours()} o'clock. {greeting}
-        {isOpen ? ", We're open" : ", We're closed"}
-      </p>
+      {isOpen ? (
+        <Order greeting={greeting} closeHours={closeHours} />
+      ) : (
+        <p>
+          Come back later between {openHours} and {closeHours}
+        </p>
+      )}
     </footer>
   );
 }
 
 function App() {
   return (
-    <>
+    <div className="layout">
       <Header />
       <Menu />
       <Footer />
-    </>
+    </div>
   );
 }
 
